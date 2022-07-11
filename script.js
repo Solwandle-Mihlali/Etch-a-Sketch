92866e2df937c9a85d152330b2fd61f783d0c47d
Mihlali Solwandle <mihlalisolwadle705@gmail.com>
2022-07-11 16:06:17 +0200

    fixed external js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let container= document.getElementById('container');
 
-const div = document.createElement('div');
+            const container = document.getElementById('container');
+            const div = document.createElement('div');
+            div.id = "gridBlock";
+            
+            function containerStyle()
+            {
+                container.style.borderStyle = "solid";
+                container.style.borderWidth = "5px"
+                container.style.minHeight = "80vh";
+                container.style.height = "80vh"
+                container.style.width= "60%";
+                container.style.display = "flex";
+                container.style.alignItems = "center";
+                container.style.justifyContent="center";
+            }
+            
+            containerStyle();
+       
 
-divCount = 0;
