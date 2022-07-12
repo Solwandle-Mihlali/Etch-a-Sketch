0a35718a3b2d3298ad7c76cacf29d80fa0555b50
Mihlali Solwandle <mihlalisolwadle705@gmail.com>
2022-07-12 09:33:06 +0200

    final commit

--- script.js
+++ script.js
@@ -1,58 +1,24 @@
-
-            const header = document.getElementById('head');
-            const container = document.getElementById('container');
-            const div = document.createElement('div');
-            
-            const blackBtn = document.createElement('button');
-            const greyBtn = document.createElement('button');
-            const rgbBtn = document.createElement('button');
-            const btnSize = document.createElement('button');
-            
-
-            blackBtn.className = "button";
-            greyBtn.className = "button";
-            rgbBtn.className = "button";
-            btnSize.className = "button";
- 
-            header.append(blackBtn,greyBtn,rgbBtn,btnSize)
-
-            function headerStyle(){
-              
-                header.style.minHeight = "10vh";
-                header.style.width = "60%";
-                header.style.border = "solid"
-                header.style.display = "flex"
-                header.style.alignItems = "center"
-                header.style.justifyContent = "center"
-                header.style.gap="30px"
-                header.style.background = "red"
-            }
-            headerStyle();
-            //defining container styles
-            function containerStyle()
-            {
-                container.style.borderStyle = "solid";
-                container.style.borderWidth = "5px"
-                container.style.minHeight = "80vh";
-                container.style.height = "80vh"
-                container.style.width= "60%";
-                container.style.display = "grid";
-                container.style.alignItems = "center";
-                container.style.justifyContent="center";
-                container.style.boxShadow ="10px 4px 10px black"
-            }
-            containerStyle();
-
-            function createGrids(col,row){
-
-                for(let x = 0; x < (col * row); x++){
-                     
-                    container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
-                    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-                    container.appendChild(div).classList.add('box')
-                }
-            }
-            createGrids(16,16);
+function createPixels(size) {
+    let container = document.querySelector(".container");
+    for (currentPixel = 0; currentPixel < size * size; currentPixel++) {
+      let pixel = document.createElement("div");
+      pixel.setAttribute("class", "pixel");
+      pixel.addEventListener("pointerover", addRgb);
+      container.appendChild(pixel);
+    }
+  }
+  
+  createPixels(20);
+  
+  function addRgb(e) {
+    e.target.style = `background-color: rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 255)})`;
+    e.target.removeEventListener("pointerover", addRgb);
+  }
+  
+  function resetContainer() {
+    let allPixels = document.querySelectorAll(".pixel");
+    allPixels.forEach(pixel => pixel.parentNode.removeChild(pixel));
+  }
 
 
             
